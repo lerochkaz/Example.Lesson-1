c9f256bd39c058702b2a646a8e68b3fd734b285a
Zazhiwihina Valeria <zazhiwihina@yandex.ru>
2022-09-15 16:41:14 +0400

    Добавили папку Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя на латинице: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "masha")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.Write(userName);
+}
