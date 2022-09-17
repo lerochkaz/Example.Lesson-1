ae9a73b468ed27aac0c33e76e2863a04f0cdbf82
Zazhiwihina Valeria <zazhiwihina@yandex.ru>
2022-09-18 00:50:34 +0400

    Добавили папку Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пять чисел: ");
+int a1 = int.Parse(Console.ReadLine());
+int a2 = int.Parse(Console.ReadLine());
+int a3 = int.Parse(Console.ReadLine());
+int a4 = int.Parse(Console.ReadLine());
+int a5 = int.Parse(Console.ReadLine());
+int[] array = { a1, a2, a3, a4, a5 };
+Console.WriteLine("Введите число, которое необходимо найти: ");
+int find = int.Parse(Console.ReadLine());
+int n = array.Length;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Индекс числа, которого мы ищем {index}");
+        break;
+    }
+    index++;
+}
